212be712e9b84848d5b5885bfc9e86874bd05d18
Abrikozs <Abrikozs@gmail.com>
2025-05-02 23:38:13 +0300

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Роман Пшеничников on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
