13604adce0de790f442a7a4ad6a710164ca395de
Abrikozs <Abrikozs@gmail.com>
2025-05-03 05:05:41 +0300

    Initial project setup with Firebase integration

--- FirebaseBootcamp/FirebaseBootcampApp.swift
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAnalytics
 
 @main
 struct FirebaseBootcampApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+// MARK: - Firebase Configuration
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Firebase configured")
+            
+            Analytics.logEvent("test_event", parameters: [
+                "user_name": "Roma",
+                "debug_mode": true
+            ])
+            
+    return true
+  }
 }
